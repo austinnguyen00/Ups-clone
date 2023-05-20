e3a05eeeac190c731813e78b54f5546f9661ff97
Austin <ndkhiem3010@gmail.com>
2023-05-20 23:08:03 +0800

    Initializing CustomerScreen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
+import CustomerScreen from './screens/CustomerScreen';
 
 export default function App() {
 	return (
 		// @ts-ignore - TailwindProvider is missing a type definition
 		<TailwindProvider utilities={utilities}>
-			<View style={styles.container}>
-				<Text>Open up App.tsx to start working on your app!</Text>
-				<StatusBar style='auto' />
-			</View>
+			<CustomerScreen />
 		</TailwindProvider>
 	);
 }
